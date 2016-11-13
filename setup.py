c89c5a2b8bd5c096b85a73e09e68a8068a6de6c2
realityone <realityone@me.com>
2016-11-13 17:05:31 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+with open(os.path.join(SOURCE_DIR, 'requirements.txt'), 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="docker-image-py",
+    version='0.1',
+    description="Parse docker image as distribution does.",
+    url='https://github.com/realityone/docker-image-py',
+    packages=['docker_image'],
+    install_requires=requirements,
+    zip_safe=False,
+)
