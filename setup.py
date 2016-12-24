93f0f27df826537533515a4f6a643a2312fcb273
realityone <realityone@me.com>
2016-12-25 00:31:35 +0800

    fix package to pypi

--- setup.py
+++ setup.py
@@ -6,15 +6,16 @@ from setuptools import setup
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
-with open(os.path.join(SOURCE_DIR, 'requirements.txt'), 'r') as f:
-    requirements = f.read().splitlines()
+install_requires = [
+    'regex>=2016.10.22',
+]
 
 setup(
     name="docker-image-py",
-    version='0.1.2',
+    version='0.1.3',
     description="Parse docker image as distribution does.",
     url='https://github.com/realityone/docker-image-py',
     packages=['docker_image'],
-    install_requires=requirements,
+    install_requires=install_requires,
     zip_safe=False,
 )
